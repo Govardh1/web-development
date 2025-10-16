dc6c88599d738971d0faeda8fb0a2a3a1104fa07
Govardhan K G <govi.reddy420@gmail.com>
2025-10-16 17:44:35 +0530

    state and comaponents

--- OneDrive/Desktop/FrontEnd/BasicReact/src/App.jsx
+++ OneDrive/Desktop/FrontEnd/BasicReact/src/App.jsx
@@ -1,36 +1,25 @@
 import { useState } from "react"
 
 
-function App() {
+function App(){
 
-  return (
-    <div style={{display:"flex", justifyContent:"space-evenly"}}>
-      <ToggleMassage /><br />
-      <ToggleMassage /><br />
-      <ToggleMassage /><br />
-      <ToggleMassage /><br />
-
-    </div>
-  )
+  const[count,setCount]=useState(0)
+ 
+return(
+  <div>
+    <CustomButton count={count} setCount={setCount}/>
+  </div>
+)
 }
+  function CustomButton(props){
+    function onclicHandler(){
+        props.setCount(props.count+1)
+      }
+     return(
+       <button onClick={onclicHandler}>counter is {props.count}</button>
+     )
 
-function ToggleMassage() {
-  const [incrementCount, setIncrementCount] = useState(0)
-
-  function increment() {
-    setIncrementCount(incrementCount + 1)
   }
-
-  return (
-    <div >
-      <button style={{ height: 200, width: 200, borderRadius: 100, fontSize: 30 }} onClick={increment}>
-        increment coint is  <span style={{color:"red"}}>{incrementCount}</span>
-      </button>
-      
-    </div>
-  )
-}
-
 
 
 export default App

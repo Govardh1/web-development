0d689d9ad5d4c5f0041fd903b6a6012062c568b1
Govardhan K G <govi.reddy420@gmail.com>
2025-10-17 12:13:56 +0530

    wrapper components

--- OneDrive/Desktop/FrontEnd/BasicReact/src/App.jsx
+++ OneDrive/Desktop/FrontEnd/BasicReact/src/App.jsx
@@ -11,29 +11,43 @@ function App() {
 
 
 const HeaderwithButton = () => {
-  const [FirstTitile, setFirstTitle] = useState("karan")
+  const [todo, changeTodo] = useState([{
+    title:"go to hyh",
+    discription:"hello"
+  },{
+    title:"go to school",
+    discription:"lekjdfh"
+  },{
+    title:"go to kargil",
+    discription:"2oeiwru"
+  },{
+    title:"go to job",
+    discription:"w;oeirjwieds"
+  }])
   function ChangeButton() {
-    setFirstTitle("my name is " + Math.random())
+   changeTodo([...todo,{
+    title:Math.random(),
+    discription:Math.random()
+   }])
   }
   return (
     <div>
-      <button onClick={ChangeButton}>click me to change</button>
-      <Header title={FirstTitile}></Header>
-      <Header title={"hello"}></Header>
-      <Header title={"hello"}></Header>
-      <Header title={"hello"}></Header>
-      <Header title={"hello"}></Header>
-      <Header title={"hello"}></Header>
-
+     <button onClick={ChangeButton}>add Todo</button>
+      {todo.map((item,index)=>
+        <Todo 
+        key={index}
+        title={item.title} 
+        discription={item.discription}/>
+      )}
     </div>
   )
 }
-const Header = React.memo(({ title ,discription}) => {
+const Todo = React.memo(({ title ,discription}) => {
   return (
     <div>
-      {title}
-      {discription}
 
+     <h1> {title}</h1>
+     <h3> {discription}</h3>
     </div>
   )
 })
